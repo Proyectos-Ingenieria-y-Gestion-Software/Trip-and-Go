c6ad2422b8b087a754c10a3139db38b58e630a34
Fernando Sanfiel Reyes <72128771+reyesanfer@users.noreply.github.com>
2023-05-05 23:45:27 +0100

    ci: :rocket: Se añaden dependencias para firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,22 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { initializeApp } from "firebase/app"
 
 export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDZfdV310zlZ5uLJzEoZmQg21Se6qW8Vac",
+    authDomain: "tripandgo-8fee3.firebaseapp.com",
+    projectId: "tripandgo-8fee3",
+    storageBucket: "tripandgo-8fee3.appspot.com",
+    messagingSenderId: "49272995284",
+    appId: "1:49272995284:web:479254baf184f193f12a41",
+    measurementId: "G-FNN8HQ2509"
+  },
   production: false
 };
 
+export const app = initializeApp(environment.firebaseConfig);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
