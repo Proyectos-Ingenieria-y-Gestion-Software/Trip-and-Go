b635af8831c5c36676e4c7bc3754afeaa0d70950
FerPellicer <fernandopellicer782@gmail.com>
2023-05-08 19:53:09 +0100

    Adelantando Home
    Primer commit home con scroll y varios componentes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Place } from 'src/app/interfaces/place';
+import { PlacesService } from 'src/app/services/places-service.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +10,25 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private navController: NavController) {}
+  places?: Place[];
+  placeGroups?: Place[][];
+
+  slideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 20,
+    centeredSlides: true
+  };
+
+  constructor(private navController: NavController, private placesService: PlacesService) {}
+
+  ngOnInit() {
+    this.placesService.getPlaces().subscribe(places => {
+      this.places = places
+    })
+  }
 
   goPlaces() {
     this.navController.navigateForward('places');
-  }
+  }  
 
 }
