4452cf193e718dd50913ffdd5cb2e617b6f41ed9
AlbertoFidalgo <albertofidalgogc@gmail.com>
2023-05-09 22:38:45 +0100

    Work done on FQA page

--- src/app/fqa/fqa.module.ts
+++ src/app/fqa/fqa.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { FQAPageRoutingModule } from './fqa-routing.module';
-
-import { FQAPage } from './fqa.page';
+import { FQAPage } from './fqa.page'; // <--- Aquí importa correctamente la clase FQAPage
 
 @NgModule({
   imports: [
