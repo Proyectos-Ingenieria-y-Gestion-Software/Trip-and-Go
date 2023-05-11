eb9845edfa9da95f8cecdae27c9c89ca6bdfc100
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2023-05-11 19:31:43 +0100

    fix: :bug: arreglo en el login

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { InicioPageRoutingModule } from './inicio-routing.module';
+import { LoginPageRoutingModule } from './login-routing.module';
 
-import { InicioPage } from './inicio.page';
+import { LoginPage } from './login.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    InicioPageRoutingModule
+    LoginPageRoutingModule
   ],
-  declarations: [InicioPage]
+  declarations: [LoginPage]
 })
-export class InicioPageModule {}
+export class LoginPageModule {}
