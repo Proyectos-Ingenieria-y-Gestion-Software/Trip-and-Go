1ea6a22e984cff1f8d2f578ba893f02563f83759
FerPellicer <fernandopellicer782@gmail.com>
2023-05-16 11:38:40 +0100

    FQA finalizado

--- src/app/pages/fqa/fqa.page.ts
+++ src/app/pages/fqa/fqa.page.ts
@@ -1,53 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { ViewChild, ElementRef } from '@angular/core';
-import { GlobalDataService } from 'src/app/services/global-data.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fqa',
   templateUrl: 'fqa.page.html',
   styleUrls: ['fqa.page.scss'],
 })
-export class FQAPage implements OnInit {
+export class FQAPage {
+  isCardExpanded1 = false;
+  isCardExpanded2 = false;
+  isCardExpanded3 = false;
 
-  @ViewChild('square', {static: true}) square!: ElementRef;
-
-  constructor(private globalData: GlobalDataService) { }
-
-  ngOnInit(): void {
-    this.globalData.setVisibleComponents(true);
+  toggleCard1() {
+    this.isCardExpanded1 = !this.isCardExpanded1;
   }
 
-  adjustSquareHeight() {
-    const squareEl = this.square.nativeElement;
-    const textEl = squareEl.querySelector('.text');
-    squareEl.style.maxHeight = `${textEl.offsetHeight + 20}px`;
-}
+  toggleCard2() {
+    this.isCardExpanded2 = !this.isCardExpanded2;
+  }
 
-  showText1 = false;
-  showText2 = false;
-  showText3 = false;
-  showText4 = false;
-
-  showText(squareNumber: number) {
-    switch (squareNumber) {
-      case 1:
-        this.showText1 = !this.showText1;
-        break;
-      case 2:
-        this.showText2 = !this.showText2;
-        break;
-      case 3:
-        this.showText3 = !this.showText3;
-        break;
-      case 4:
-        this.showText4 = !this.showText4;
-        break;
-      default:
-        break;
-    }
-
-    setTimeout(() => {
-      this.adjustSquareHeight();
-    }, 0);
+  toggleCard3() {
+    this.isCardExpanded3 = !this.isCardExpanded3;
   }
 }
