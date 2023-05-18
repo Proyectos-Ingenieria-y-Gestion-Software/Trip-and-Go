9264b16107c0f09e8d102532dbb8aacbeeeb82f5
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2023-05-18 13:53:12 +0100

    feat: :sparkles: muestra de lista de deseos

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalDataService } from 'src/app/services/global-data.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NavigationExtras } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { EventsService } from 'src/app/services/events-service.service';
+import { Event } from 'src/app/interfaces/event';
 
 @Component({
   selector: 'app-profile',
@@ -11,11 +15,20 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 export class ProfilePage implements OnInit {
 
   user: any ={ };
+  events?: Event = { };
 
-  constructor(private globalData: GlobalDataService, private auth: AngularFireAuth, private firestore: AngularFirestore) {}
+
+  constructor(private globalData: GlobalDataService, private auth: AngularFireAuth, private firestore: AngularFirestore, 
+    private navCtrl: NavController,private eventsService: EventsService,) {}
 
   ngOnInit() {
     this.globalData.setVisibleComponents(true);
+
+    this.eventsService.getEventById('JvUDCS7BogIQ4i63ViLY').then(observablePromise => {
+      observablePromise.subscribe(data => {
+        this.events = data;
+      });
+    });
 
     this.auth.user.subscribe(user => {
       if (user) {
@@ -29,6 +42,15 @@ export class ProfilePage implements OnInit {
         });
       }
     });
+  }
+
+  viewEvent(event: Event) {
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        event
+      }
+    };
+    this.navCtrl.navigateForward('event-details', navigationExtras);
   }
 
   logOut(){
